6fc7151437275dd81db3b9d302a073f88db79deb
Morteza Farrokhnejad <152209810+IAmFarrokhnejad@users.noreply.github.com>
2024-11-30 19:50:50 +0300

    Video script generation done.

--- clipcraft/app/api/get-video-script/route.jsx
+++ clipcraft/app/api/get-video-script/route.jsx
@@ -3,14 +3,14 @@ import { NextResponse } from "next/server";
 
 export async function POST(req) {
     try {
-        const {prompt} = await req.json()
+        const { prompt } = await req.json()
         console.log(prompt);
 
         const result = await chatSession.sendMessage(prompt);
         console.log(result.response.text());
 
-        return NextResponse.json({'Result': JSON.parse(esult.response.text())})
-    } catch(e) {
-        return NextResponse.json({'Error: ': e})
+        return NextResponse.json({ 'Result': JSON.parse(esult.response.text()) })
+    } catch (e) {
+        return NextResponse.json({ 'Error: ': e })
     }
 }
--- clipcraft/app/dashboard/create-new/page.jsx
+++ clipcraft/app/dashboard/create-new/page.jsx
@@ -4,17 +4,39 @@ import SelectTopic from "./_components/SelectTopic"
 import SelectStyle from "./_components/SelectStyle";
 import SelectDuration from "./_components/SelectDuration";
 import { Button } from "@/components/ui/button";
+import CustomLoading from "./_components/CustomLoading";
 
 function CreateNew() {
 
   const [formData, setFormData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [videoScript, setVideoScript] = useState();
+
   const onHandleInputChange = (fieldName, filedValue) => {
 
-    setFormData(prev =>({
+    setFormData(prev => ({
       ...prev,
       [fieldName]: filedValue
     }))
   }
+
+  const onCreateClickHandler = () => {
+    getVideoScript();
+  }
+
+  //Get video script
+  const getVideoScript = async () => {
+    setLoading(true)
+    const prompt = 'Write a script to generate a ' + formData.duration + ' video on the following topic: ' + formData.topic + ' along with AI image prompt in ' + formData.imageStyle + ' format for each scene and give the result in JSON format with imagePrompt and ContentText as field'
+    const result = await axios.post('/api/get-video-script', {
+      prompt: prompt
+    }).then(resp => {
+      console.log(resp.data.result);
+      setVideoScript(resp.data.result);
+    })
+    setLoading(false)
+  }
+
   return (
     <div className="md:px-20">
       <h2 className="font-bold text-4xl text-primary  text-center">Create New</h2>
@@ -25,14 +47,15 @@ function CreateNew() {
         <SelectTopic onUserSelect={onHandleInputChange} />
 
         {/*Select Style*/}
-        <SelectStyle onUserSelect={onHandleInputChange}/>
+        <SelectStyle onUserSelect={onHandleInputChange} />
         {/*Duration*/}
-        <SelectDuration onUserSelect={onHandleInputChange}/>
+        <SelectDuration onUserSelect={onHandleInputChange} />
 
         {/*Create Button*/}
-        <Button className="mt-10 w-full">Create Video</Button>
-
+        <Button className="mt-10 w-full" onClick={onCreateClickHandler}>Create Video</Button>
       </div>
+
+      <CustomLoading loading={loading} />
     </div>
   )
 }
