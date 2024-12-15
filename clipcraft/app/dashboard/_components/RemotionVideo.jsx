7e52c3ec95c1c8a8f9c8e3434cd9fefa2edd164d
Morteza Farrokhnejad <152209810+IAmFarrokhnejad@users.noreply.github.com>
2024-12-16 00:14:12 +0300

    Animations for videos

--- clipcraft/app/dashboard/_components/RemotionVideo.jsx
+++ clipcraft/app/dashboard/_components/RemotionVideo.jsx
@@ -1,11 +1,11 @@
-import { AbsoluteFill, Sequence, useVideoConfig, Img, Audio, useCurrentFrame } from 'remotion';
+import { AbsoluteFill, Sequence, useVideoConfig, Img, Audio, useCurrentFrame, interpolate } from 'remotion';
 
 function RemotionVideo(script, imageList, audioFileUrl, captions, setDurationInFrame) {
 
     const { fps } = useVideoConfig();
     const frame = useCurrentFrame();
 
-    const getDurationFrames = () => {
+    const getDurationFrame = () => {
         setDurationInFrame(captions[captions?.length - 1]?.end / 1000 * fps)
         return captions[captions?.length - 1]?.end / 1000 * fps;
     }
@@ -14,35 +14,48 @@ function RemotionVideo(script, imageList, audioFileUrl, captions, setDurationInF
         const currentTime = frame / 30 * 1000;
         const currentCaption = captions.find((word) => currentTime >= word.start && currentTime <= word.end)
 
-        return currentCaption?currentCaption?.text : '';
+        return currentCaption ? currentCaption?.text : '';
     }
 
     return (
         <AbsoluteFill classname="bg-black">
-            {imageList?.map((item, index) => (
-                <>
-                    <Sequence key={index} from={((index * getDurationFrames()) / imageList?.length)} durationInFrames={getDurationFrames()}>
-                        <AbsoluteFill style={{ justifyContent:'center', alignItems: 'center' }}>
-                            <Img src={item} style={{
-                                width: '100%',
-                                height: '100%',
-                                objectFit: 'cover'
-                            }} />
-                            <AbsoluteFill style={{
-                                color: white,
-                                justifyContent: 'center',
-                                top: undefined,
-                                bottom: 50,
-                                height: 150,
-                                textAlign: 'center',
-                                width: '100%'
-                            }}>
-                                <h2 className='text-2xl'>{getCurrentCaptions()}</h2>
+            {imageList?.map((item, index) => {
+
+                const startTime = ((index * getDurationFrame()) / imageList?.length)
+                const duration = getDurationFrame();
+
+                const scale = (index) => interpolate(
+                    frame,
+                    [startTime, startTime + duration/2, startTime+duration], //For zooming in and zooming out
+                    index%2 === 0 ? [1, 1.8,1] : [1.8, 1, 1.8],
+                    {extrapolateLeft: 'clamp', extrapolateRight: 'clamp'}
+                )
+                return (
+                    <>
+                        <Sequence key={index} from={startTime} durationInFrames={getDurationFrames()}>
+                            <AbsoluteFill style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                <Img src={item} style={{
+                                    width: '100%',
+                                    height: '100%',
+                                    objectFit: 'cover',
+                                    transform: `scale(${scale(index)})`
+                                }} />
+                                <AbsoluteFill style={{
+                                    color: white,
+                                    justifyContent: 'center',
+                                    top: undefined,
+                                    bottom: 50,
+                                    height: 150,
+                                    textAlign: 'center',
+                                    width: '100%'
+                                }}>
+                                    <h2 className='text-2xl'>{getCurrentCaptions()}</h2>
+                                </AbsoluteFill>
                             </AbsoluteFill>
-                        </AbsoluteFill>
-                    </Sequence>
-                </>
-            ))}
+                        </Sequence>
+                    </>
+                )
+            })}
             <Audio src={audioFileUrl} />
         </AbsoluteFill>
     )
